7082cf0897d3cbb24fb0df63f6b3631da9404bdb
Manuel Paez <manoletete8@gmail.com>
2021-10-03 17:36:55 -0300

    DAO

---
+++ app/src/main/java/com/papeliyodev/movieapp/data/local/MovieDao.kt
@@ -0,0 +1,18 @@
+package com.papeliyodev.movieapp.data.local
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.papeliyodev.movieapp.data.model.MovieEntity
+
+@Dao
+interface MovieDao {
+
+    @Query("SELECT * FROM MovieEntity")
+    suspend fun getAllMovies(): List<MovieEntity>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun saveMovie(movie: MovieEntity)
+
+}
